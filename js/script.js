f5ef2e2ba08650df5dfa6409176e4b888653b342
einehc <ningas.chenie@gmail.com>
2022-10-28 12:22:32 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+function converttousd () {
+    let x = document.getElementById ("input").value;
+    const y = 0.017;
+    const input = parseFloat(x) * parseFloat(y);
+    alert ("The conversion is " + input); 
+   }
+
+   function converttoyen () {
+    let x = document.getElementById ("input").value;
+    const y = 2.52;
+    const input = parseFloat(x) * parseFloat(y);
+    alert ("The conversion is " + input);
+   }
+
+   function converttowon () {
+    let x = document.getElementById ("input").value;
+    const y = 24.38;
+    const input = parseFloat(x) * parseFloat(y);
+    alert ("The conversion is " + input);
+
+   }
+
+
