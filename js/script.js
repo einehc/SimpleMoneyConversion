50aa0278aa4f2b76a01d133f31706daab7b20246
einehc <ningas.chenie@gmail.com>
2022-10-28 17:59:39 +0800

    update display

--- js/script.js
+++ js/script.js
@@ -3,21 +3,25 @@ function converttousd () {
     let x = document.getElementById ("input").value;
     const y = 0.017;
     const input = parseFloat(x) * parseFloat(y);
-    alert ("The conversion is " + input); 
+    document.getElementById("display").innerHTML=input;
+    document.getElementById("displayUnit").innerHTML= "USD";
+
    }
 
    function converttoyen () {
     let x = document.getElementById ("input").value;
     const y = 2.52;
     const input = parseFloat(x) * parseFloat(y);
-    alert ("The conversion is " + input);
+    document.getElementById("display").innerHTML=input;
+    document.getElementById("displayUnit").innerHTML= "Yen";
    }
 
    function converttowon () {
     let x = document.getElementById ("input").value;
     const y = 24.38;
     const input = parseFloat(x) * parseFloat(y);
-    alert ("The conversion is " + input);
+    document.getElementById("display").innerHTML=input;
+    document.getElementById("displayUnit").innerHTML= "Won";    
 
    }
 
